433a6f9404de69780bbdaf858a2cb7753d8fdc30
Bernadette Elena Hammerle <bernadette.elena.hammerle@gmail.com>
2024-11-10 22:59:50 +0100

    WIP: clean up footer; update header links; add headings

--- boudicca.events/publisher-event-html/src/main/kotlin/events/boudicca/publisher/event/html/StaticSitesController.kt
+++ boudicca.events/publisher-event-html/src/main/kotlin/events/boudicca/publisher/event/html/StaticSitesController.kt
@@ -36,8 +36,6 @@ class BoudiccaEventsExtension : Extension {
     override fun getHeaders(): List<HeaderExtension> {
         return listOf(
             HeaderExtension("Über uns", "/about"),
-            HeaderExtension("Impressum", "/impressum"),
-            HeaderExtension("GitHub", "https://github.com/boudicca-events/boudicca.events", "_blank"),
         )
     }
 }
